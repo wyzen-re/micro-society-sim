1785bc652d8e4cd161d3304bd26813aebfdfac64
wyzen-re <monkeydzenso74@gmail.com>
2026-01-22 20:19:55 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,1 +1,10 @@
+from society import Society
 
+GENERATIONS = 10
+
+society = Society(population_size=20)
+
+for gen in range(GENERATIONS):
+    society.form_groups()
+    society.interactions()
+    society.summary(gen)
