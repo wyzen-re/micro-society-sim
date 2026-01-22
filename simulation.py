22a6f153d358ede01b8d5cb71a32e7e29159dad5
wyzen-re <monkeydzenso74@gmail.com>
2026-01-22 20:42:56 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,10 +1,13 @@
 from society import Society
 
-GENERATIONS = 10
+GENERATIONS = 30
 
-society = Society(population_size=20)
+society = Society(population_size=20, world_size=10)
 
 for gen in range(GENERATIONS):
+    print(f"\n--- Generation {gen} ---")
     society.form_groups()
-    society.interactions()
-    society.summary(gen)
+    society.step()
+    society.conflict()
+    society.world.regenerate()
+    society.summary()
